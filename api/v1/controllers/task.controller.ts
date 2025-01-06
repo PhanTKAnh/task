a49c79656933e6af9a3c1b8bbe5e05b83348e96a
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2025-01-06 15:56:30 +0700

    sap xep task

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req:Request, res:Response) =>{
+    // FIND
     interface Find{
         deleted: boolean,
         status?: string
@@ -13,8 +14,17 @@ export const index = async (req:Request, res:Response) =>{
     if(req.query.status){
         find.status= req.query.status.toString();
     }
+    //END FIND
 
-    const tasks = await Task.find(find);
+    // Sort
+    const sort = {};
+
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey = req.query.sortKey.toString();
+        sort[sortKey] = req.query.sortValue;
+    }
+    // End sort
+    const tasks = await Task.find(find).sort(sort);
 
     res.json(tasks)
 }
