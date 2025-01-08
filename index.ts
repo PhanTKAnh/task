72f449b84c74e1992b8d78a8243282f3e8c1995b
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2025-01-08 23:04:01 +0700

    tao cors

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express,{ Express, Request, Response} from "express"; 
 import dotenv from "dotenv"
+import cors from "cors"; 
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/router/index.route";
 
@@ -10,6 +11,16 @@ database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+
+
+// const  corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+
+//   app.use(cors(corsOptions));
+
+  app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
